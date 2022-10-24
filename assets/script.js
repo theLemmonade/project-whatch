90a3ecf717a65c53610faf15b1a74e46cf10c283
theLemmonade <lemmonmail@icloud.com>
2022-10-24 19:44:30 -0400

    quiz1 built

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+const opening = document.getElementById("opening")
+const quiz0 = document.getElementById("quiz0")
+const quiz1 = document.getElementById("quiz1")
+const quiz2 = document.getElementById("quiz2")
+const results = document.getElementById("results")
 var genre = String
 var isMovie = Boolean
 var suggestion
@@ -8,42 +13,44 @@ var streams = {
 }
 
 function showOpening() {
-  document.getElementById("opening").setAttribute("style", "display:block");
-  document.getElementById("quiz0").setAttribute("style", "display:none");
-  document.getElementById("quiz1").setAttribute("style", "display:none");
-  document.getElementById("quiz2").setAttribute("style", "display:none");
-  document.getElementById("results").setAttribute("style", "display:none");
+  opening.setAttribute("style", "display:block");
+  quiz0.setAttribute("style", "display:none");
+  quiz1.setAttribute("style", "display:none");
+  quiz2.setAttribute("style", "display:none");
+  results.setAttribute("style", "display:none");
 }
 
 function showQuiz0() {//what genre do you like?
-    document.getElementById("opening").setAttribute("style", "display:none");
-    document.getElementById("quiz0").setAttribute("style", "display:block");
-    document.getElementById("quiz1").setAttribute("style", "display:none");
-    document.getElementById("quiz2").setAttribute("style", "display:none");
-    document.getElementById("results").setAttribute("style", "display:none");
-    document.getElementById("romance").addEventListener("click", genre = "romance", console.log(genre), showQuiz1);
-    document.getElementById("horror").addEventListener("click", genre = "horror", console.log(genre), showQuiz1);
-    document.getElementById("comedy").addEventListener("click", genre = "comedy", console.log(genre), showQuiz1);
-    document.getElementById("action").addEventListener("click", genre = "action", console.log(genre), showQuiz1);
-    document.getElementById("international").addEventListener("click", genre = "international", console.log(genre), showQuiz1);
+  opening.setAttribute("style", "display:none");
+  quiz0.setAttribute("style", "display:block");
+  quiz1.setAttribute("style", "display:none");
+  quiz2.setAttribute("style", "display:none");
+  results.setAttribute("style", "display:none");
+  document.getElementById("romance").addEventListener("click", genre = "romance", console.log(genre), showQuiz1);
+  document.getElementById("horror").addEventListener("click", genre = "horror", console.log(genre), showQuiz1);
+  document.getElementById("comedy").addEventListener("click", genre = "comedy", console.log(genre), showQuiz1);
+  document.getElementById("action").addEventListener("click", genre = "action", console.log(genre), showQuiz1);
+  document.getElementById("international").addEventListener("click", genre = "international", console.log(genre), showQuiz1);
 }
 
 function showQuiz1() {//movie or series?
-  document.getElementById("opening").setAttribute("style", "display:none");
-  document.getElementById("quiz0").setAttribute("style", "display:none");
-  document.getElementById("quiz1").setAttribute("style", "display:block");
-  document.getElementById("quiz2").setAttribute("style", "display:none");
-  document.getElementById("results").setAttribute("style", "display:none");
-  document.getElementById("movie").addEventListener("click", genre = "action", console.log(genre), showQuiz1);
-  document.getElementById("series").addEventListener("click", genre = "international", console.log(genre), showQuiz1);
+  opening.setAttribute("style", "display:none");
+  quiz0.setAttribute("style", "display:none");
+  quiz1.setAttribute("style", "display:block");
+  quiz2.setAttribute("style", "display:none");
+  results.setAttribute("style", "display:none");
+  document.getElementById("movie").addEventListener("click", isMovie = true, console.log(isMovie), showQuiz2);
+  document.getElementById("series").addEventListener("click", isMovie = false, console.log(isMovie), showQuiz2);
 }
 
-function showQuiz2() {
+function showQuiz2() {//what platforms do you have?
   document.getElementById("opening").setAttribute("style", "display:none");
   document.getElementById("quiz0").setAttribute("style", "display:none");
   document.getElementById("quiz1").setAttribute("style", "display:none");
   document.getElementById("quiz2").setAttribute("style", "display:block");
   document.getElementById("results").setAttribute("style", "display:none");
+  dpc
+  
 }
 
 function showResults() {
