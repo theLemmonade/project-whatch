897383942c7cefa9e8654c64dfebfeedd4781496
theLemmonade <lemmonmail@icloud.com>
2022-10-24 21:20:05 -0400

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,23 +43,14 @@ function showQuiz1() {//movie or series?
   document.getElementById("series").addEventListener("click", isMovie = false, console.log(isMovie), showQuiz2);
 }
 
-function showQuiz2() {//what platforms do you have? (with submit button)
+function showQuiz2() {//what decade are you into rn?
   opening.setAttribute("style", "display:none");
   quiz0.setAttribute("style", "display:none");
   quiz1.setAttribute("style", "display:none");
   quiz2.setAttribute("style", "display:block");
   results.setAttribute("style", "display:none");
-  document.getElementById("netflix").addEventListener("click", streams.netflix = true);
-  document.getElementById("amazon").addEventListener("click", streams.amazon = true);
-  document.getElementById("hulu").addEventListener("click", streams.hulu = true);
-  function submitQuiz2() {
-    if (streams.netflix && streams.amazon && streams.hulu == false) {
-      document.getElementById("warning").textContent("Please select atleast one streaming platform.")
-    } else {
-      showResults
-    }
-  }
-  document.getElementById("submitQuiz2").addEventListener("click", submitQuiz2);
+  
+
 }
 
 function showResults() {//quiz results
