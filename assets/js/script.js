0f555a147849eb22957f9d6403624f1a729a9c16
theLemmonade <lemmonmail@icloud.com>
2022-10-31 19:26:11 -0400

    added no trailer fallback operation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Variables
 // DOM Element References
 const opening = document.getElementById("opening");
+const begin = document.getElementById("begin")
 const quiz0 = document.getElementById("quiz0");
 const quiz1 = document.getElementById("quiz1");
 const quiz2 = document.getElementById("quiz2");
@@ -14,6 +15,8 @@ const desc = document.getElementById("desc");
 const date = document.getElementById("date");
 const rating = document.getElementById("rating");
 const search = document.getElementById("search");
+const ytcont = document.getElementById("ytcontainer")
+const reload = document.getElementById("reset")
 // Quiz Selectors
 var genre = String;
 var isMovie = Boolean;
@@ -284,6 +287,7 @@ function fetchTrailerID() {
           trailerKey = data.results[i].key;
           console.log(trailerKey);
         } else {
+          ytcont.setAttribute("style", "display:none");
           console.log("no trailer for you");
         }
       }
@@ -337,6 +341,6 @@ function reset () {
   location.reload()
 }
 
-document.getElementById("begin").addEventListener("click", showQuiz0);
-document.getElementById("reset").addEventListener("click", reset);
+begin.addEventListener("click", showQuiz0);
+reload.addEventListener("click", reset);
 showOpening();
